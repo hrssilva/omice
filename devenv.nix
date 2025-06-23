c3bbc48e4ee6b86a084bbef113a227f84e9c2c86
hrssilva <haroldo.rssilva@gmail.com>
2025-06-22 22:57:49 -0300

    start to dealing with qualities

--- devenv.nix
+++ devenv.nix
@@ -1,6 +1,7 @@
 # devenv.nix
 { pkgs, ... }:
 
+
 let
   my-python = pkgs.python312.withPackages (ps: with ps; [
     rdflib
@@ -14,14 +15,30 @@ in
   packages = [
     my-python
     pkgs.git
+    pkgs.podman
   ];
 
   languages.python.enable = true;
   #languages.python.version = "3.12";
 
+  # Define custom commands
+  scripts = {
+    shacl-validate.exec = ''
+    podman run --rm -v ./:/data ghcr.io/ashleycaselli/shacl:latest validate -datafile /data/omice.ttl -shapesfile /data/omice.ttl
+    '';
+
+    shacl-infer.exec = ''
+    podman run --rm -v ./:/data ghcr.io/ashleycaselli/shacl:latest infer -datafile /data/omice.ttl -shapesfile /data/omice.ttl
+    '';
+  };
+
+
   enterShell = ''
     echo "🐍 Python 3.12 dev environment ready!"
     echo "✔ rdflib, matplotlib, networkx pre-installed"
+    echo "🛠 Custom commands available:"
+    echo "  - shacl-validate"
+    echo "  - shacl-infer"
   '';
 }
 
